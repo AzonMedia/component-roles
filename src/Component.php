40d6fc388211896255d74879d4dcb30931bbe4d3
Vesko Kenashkov <vesko@azonmedia.com>
2019-12-16 15:05:32 +0200

    fixed ns

--- src/Component.php
+++ src/Component.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace GuzabaPlatform\Users;
+namespace GuzabaPlatform\Roles;
 
 use GuzabaPlatform\Components\Base\Interfaces\ComponentInterface;
 use GuzabaPlatform\Components\Base\Traits\ComponentTrait;
@@ -15,11 +15,11 @@ class Component implements ComponentInterface
 
     use ComponentTrait;
 
-    protected const COMPONENT_NAME = "Users";
+    protected const COMPONENT_NAME = "Roles";
     //https://components.platform.guzaba.org/component/{vendor}/{component}
-    protected const COMPONENT_URL = 'https://components.platform.guzaba.org/component/guzaba-platform/users';
+    protected const COMPONENT_URL = 'https://components.platform.guzaba.org/component/guzaba-platform/roles';
     //protected const DEV_COMPONENT_URL//this should come from composer.json
-    protected const COMPONENT_NAMESPACE = 'GuzabaPlatform\\Users';
+    protected const COMPONENT_NAMESPACE = 'GuzabaPlatform\\Roles';
     protected const COMPONENT_VERSION = '0.0.1';//TODO update this to come from the Composer.json file of the component
     protected const VENDOR_NAME = 'Azonmedia';
     protected const VENDOR_URL = 'https://azonmedia.com';
